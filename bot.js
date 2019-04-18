34e44e7978152e5301202116e18a5096d85d137a
msg <49604348+untafx@users.noreply.github.com>
2019-04-19 06:12:38 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,23 +2,38 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
-
  
 
 client.on('ready', () => {
 
     console.log('I am ready!');
-    
+
 });
 
  
 
-if(command === "ping") {
-    // Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-    // The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
-    const m = await message.channel.send("Ping?");
-    m.edit(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`);
-  }
+client.on('message', message => {
+
+    if (message.content === '/news') {
+
+       message.reply('Last News Update In 14/4/2019 : UntaFx has new post on @untafx Instagram! :)');
+
+       }
+
+});
+
+client.on('message', message => {
+
+    if (message.content === '/about') {
+
+       message.reply('Bot made by UntaFX');
+
+       }
+
+});
+
+});
+
 
  
 
