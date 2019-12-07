a6e810c7c2e3a01e9b8168cf749a497b8b5cb4f0
msg <49604348+untafx@users.noreply.github.com>
2019-12-07 20:39:55 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,136 +3,15 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-});
- 
-
-client.on('ready', () => {
-
-    console.log('I am ready!');
-
-});
-
- 
-
-client.on('message', message => {
-
-    if (message.content === '/bot satedonk') {
-
-       message.reply('Nih satenya, silahkan dimakan');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.client === '/about') {
-
-       message.reply('Bot made by UntaFX');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'stfu') {
-
-       message.reply('Okay okay!');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'UwU') {
-
-       message.reply('OwO');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'Nigga') {
-
-       message.reply('You just say the N-WORD!!!. I will call 911!');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'Oof') {
-
-       message.reply('OOF!');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'hey') {
-
-       message.reply('tayo :V');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'ngeteh') {
-
-       message.reply('Yee.... ngeteh lah');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'gak tau males') {
-
-       message.reply('Pengen beli truck!');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'nolife') {
-
-       message.reply('No life sendiri terdiri dari dua kata yang terdapat di dalam bahasa inggris. Kata tersebut yakni "No" dan juga kata "life". Kata no sendiri didalam bahasa indonesia dapat diartikan sebagai "tidak", dan kata life dapat diartikan sebagai "hidup / kehidupan". Jadi kedua kata tersebut, secara bahasa, secara kasar dapat diartikan sebagai "Tidak hidup" atau lebih mudahnya "Tidak memiliki kehidupan". Nah, dari arti kata nolife saja kita sudah mengetahui secara kasar mengenai definisi kata nolife yang sebenarnya.');
-
-       }
-
-});
-
-client.on('message', message => {
-
-    if (message.content === 'sabar') {
-
-       message.reply('Orang sabar pantatnya lebar :v');
-
-       }
-
-});
-
-
-
-
-
-
-
- 
-
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+  
 // THIS  MUST  BE  THIS  WAY
 
 client.login(process.env.BOT_TOKEN);
