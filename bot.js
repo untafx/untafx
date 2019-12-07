0e522db7b1b9df853236247de4f8bc1dba03ffed
msg <49604348+untafx@users.noreply.github.com>
2019-12-07 20:25:40 +0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -2,6 +2,9 @@ const Discord = require('discord.js');
 
 const client = new Discord.Client();
 
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
  
 
 client.on('ready', () => {
